84b26ef2d7440603e9f9ec4b2c291c2eca2aa7d4
eory <bulawetov03@gmail.com>
2023-08-23 05:43:56 +0300

    commit message here

---
+++ source/js/header.js
@@ -0,0 +1,135 @@
+const header__search = document.querySelector('.header__search')
+const header__list = document.querySelector('.header__list')
+const header_search_closer = document.querySelector('#header-search-closer')
+const header__search_buttons_empty = document.querySelector('.header__search-buttons-empty')
+const header__search_value_del = document.querySelector('.header__search-value-del')
+const header__search_search_button = document.querySelector('.header__search-search-button')
+const body = document.querySelector('.body')
+
+function body_overflow() {
+    body.style.overflow = 'auto'
+}
+
+function header__search_value_do_empty() {
+    return header__search.value == ''
+}
+
+function header__search_buttons_remove() {
+    header__search_search_button.classList.remove('header__search-search-button_alt')
+}
+
+function header__search_buttons_empty_alt_remove() {
+    header__search_buttons_empty.classList.remove('header__search-buttons-empty_alt')
+}
+
+function header__search_value_remove() {
+    header__search.value = ''
+}
+
+function header_search_closer_full_size_remove() {
+    header__search.blur()
+    header_search_closer.classList.remove('header-search-closer_alt')
+    header__search.classList.remove('header__search_alt')
+    header__list.classList.remove('header__list_alt')
+    header_search_closer.classList.remove('header-search-closer')
+}
+
+header__search.onclick = function() { 
+    body.style.overflow = 'hidden'
+    header__search.classList.add('header__search_alt')
+    header__list.classList.add('header__list_alt')
+    header_search_closer.classList.add('header-search-closer')
+    header_search_closer.classList.add('header-search-closer_alt')
+    header__search_search_button.classList.add('header__search-search-button_alt')
+    clearTimeout(timeout)
+    slider_line_active_0()
+}
+
+header__search_search_button.onclick = function() {
+    header__search_search_button.classList.remove('header__search-search-button_alt-2')
+    setInterval(function() {
+        if (header__search_value_do_empty()) {
+            header__search_search_button.classList.add('header__search-search-button_alt-2') 
+        }
+    }, 0)
+}
+
+header__search.oninput = function() {
+    header__search_buttons_empty.classList.add('header__search-buttons-empty_alt')
+    if (header__search_value_do_empty()) header__search_buttons_empty_alt_remove()
+}
+
+header__search_value_del.onclick = function() {
+    header__search_value_remove()
+    header__search_buttons_empty_alt_remove()
+    header__search.focus()
+} 
+
+header_search_closer.onclick = function() {
+    body_overflow()
+    header__search_buttons_remove()
+    header_search_closer_full_size_remove()
+    header__search_value_remove()
+    header__search_buttons_empty_alt_remove()
+    slider_underline()
+}
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === "Escape" && header__search.classList.contains('header__search_alt')) {
+        body_overflow()
+        header__search_value_remove()
+        header_search_closer_full_size_remove()
+        header__search_buttons_remove()
+        header__search_buttons_empty_alt_remove()
+        slider_underline()
+    }
+})
+
+
+    //ripple-buttons
+let buttons = document.querySelectorAll('[data-ripple]')
+buttons.forEach(button => {
+    button.onmousedown = function(event) {
+        let coordinate = this.getBoundingClientRect()
+        const ripple = document.createElement('span')
+        const x = event.pageX - coordinate.left - 10
+        const y = event.pageY - coordinate.top - 10
+        ripple.className = 'ripple'
+        ripple.style.left = x + 'px'
+        ripple.style.top = y + 'px'
+        this.appendChild(ripple)
+        setTimeout(() => {
+            ripple.parentNode.removeChild(ripple)
+        }, 350)
+    }  
+})
+
+
+    //header swup position
+let footer = document.querySelector('.footer') 
+let header = document.querySelector('.header')
+
+const option = {
+    rootMargin: `0px 0px 200px 0px`
+}
+
+const callback_func = function(entries) {
+    entries.forEach(element => {
+        const {isIntersecting} = element
+        if (isIntersecting) {
+            Object.assign(header.style, {
+                position: 'fixed',
+                transform: 'translateY(-100px)',
+                transition: '300ms ease-in',
+            })
+        }
+        else {
+            Object.assign(header.style, {
+                transform: 'translateY(0px)',
+                position: 'sticky',
+            })
+        }
+    })
+}
+const observ = new IntersectionObserver(callback_func, option)
+observ.observe(footer)
