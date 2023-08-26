9c716b225d74c20238e4d0a1594d09f072577382
petthejir <bulawetov03@gmail.com>
2023-08-26 08:39:24 +0300

    add_card

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,7 +1,9 @@
 let categories = [...document.querySelectorAll('.main-catalog__category')]
 for (let i = 0; i < categories.length; i++) {
     let vertical = [...document.querySelectorAll('.main-catalog__category-plus-vertical')]
+    let products = [...document.querySelectorAll('.main-catalog__category-products')]
     categories[i].onclick = () => {
-        vertical[i].classList.toggle('main-catalog__category-plus-vertical_alternative')          
+        vertical[i].classList.toggle('main-catalog__category-plus-vertical_alternative')      
+        products[i].classList.toggle('main-catalog__category-products_alternative') 
     }    
 }
