74402774fabb9a363ee76cbc12f5eb119c91840c
petthejir <bulawetov03@gmail.com>
2023-09-05 19:33:18 +0300

    free day

--- js/index.js
+++ js/index.js
@@ -170,6 +170,8 @@ const header__search_buttons_empty = document.querySelector('.header__search-but
 const header__search_value_del = document.querySelector('.header__search-value-del')
 const header__search_search_button = document.querySelector('.header__search-search-button')
 
+console.log(header__search)
+
 function header__search_value_do_empty() {
     return header__search.value == ''
 }
@@ -293,14 +295,14 @@ let cards = [...document.querySelectorAll('.card')] // все карты
 let rating = document.querySelectorAll('.card__rating-value') 
 let sort_value = document.querySelector('.main-catalog__sort-value')
 let cards_array = []
+let card_discount_value = document.querySelectorAll('.card__discount-value') // value скидки
 
 for (let i = 0; i < cards.length; i++) {
     let price_new = [...document.querySelectorAll('.card__price-new')] // инициализация блока новой цены
     let price_old = [...document.querySelectorAll('.card__price-old')] // инициализация блока старой цены
     let price_old_value = price_old.map(price => price.getAttribute('data-old-price'))// value price old
-    let card_discount = document.querySelectorAll('.card__discount') // инициализация скидки
-    let card_discount_value = document.querySelectorAll('.card__discount-value') // value скидки
     let price_new_value = price_new.map(price => price.getAttribute('data-new-price')) // value price new
+    let card_discount = document.querySelectorAll('.card__discount') // инициализация скидки
     if (price_new_value[i] != '') {
         price_old[i].classList.add('card__price-old_alternative') // применение классов к old
         price_old[i].innerHTML = `<strike>${price_old[i].innerHTML}</strike>` // зачёркивание old
@@ -329,7 +331,8 @@ for (let i = 0; i < cards.length; i++) {
     //profile
 let cabinet = document.querySelector('.header__cabinet-link')
 cabinet.onclick = () => {
-    
+
+    return false
 }
 
 
