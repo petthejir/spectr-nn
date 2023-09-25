88f12e7cce2edd1c766a42de9f872a8ac25e0e26
petthejir <bulawetov03@gmail.com>
2023-09-25 20:00:37 +0300

    reg and entry

---
+++ php/connect.php
@@ -0,0 +1,10 @@
+<?
+    $conn = mysqli_connect("localhost", "root", "", "spectr");
+
+    if (!$conn) {
+        die("not connect". mysqli_connect_error()); 
+    }
+    else {
+        "connect";
+    }
+?>
